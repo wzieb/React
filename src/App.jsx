91ea90a83b4f0d08ab08bce890dc011d88da4422
wziebe <w.ziebert@gmail.com>
2024-09-07 18:30:39 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
 
-import {About}from './components/About';
-import {Contact}from './components/Contact';
-import {Footer}from './components/Footer';
-import {Header}from './components/Header';
-import {Projects}from './components/Projects';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Projects from './components/Projects';
 import './App.css';
 
 function App() {
